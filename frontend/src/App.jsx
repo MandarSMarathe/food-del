941d2271ad43d223ef395ba8826e21478df24114
MandarMarathe <153422616+MandarSMarathe@users.noreply.github.com>
2025-05-14 21:40:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer';
 import { Routes, Route } from 'react-router-dom'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
