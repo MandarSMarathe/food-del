58fe24f8f4b5dd3c3f25d163d90798bffab45b68
MandarMarathe <153422616+MandarSMarathe@users.noreply.github.com>
2025-05-14 21:46:52 +0530

    Update Verify.jsx

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import "./Verify.css"
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { useContext } from 'react';
-import { StoreContext } from '../../Context/StoreContext';
+import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
 import { useEffect } from 'react';
 
